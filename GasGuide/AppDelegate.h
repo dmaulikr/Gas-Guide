f7fb58545d5cced0e3356230824e3d328827e01c
maksym120 <maksym.m120@gmail.com>
2017-02-15 15:48:47 +0100

    Initial Commit

---
+++ GasGuide/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  GasGuide
+//
+//  Created by JCB on 2/15/17.
+//  Copyright © 2017 Mobile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ GasGuide/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GasGuide
+//
+//  Created by JCB on 2/15/17.
+//  Copyright © 2017 Mobile LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
